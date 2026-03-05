081a3a884a9ccae250789c7b5d6611a3e8f3acf1
Tommaso-Crocetti <t.crocetti1@studenti.unipi.it>
2026-03-05 22:38:29 +0100

    Mini_TyFun first implementation

--- Mini_tyfun/lib/Mini_tyfun.ml
+++ Mini_tyfun/lib/Mini_tyfun.ml
@@ -11,12 +11,12 @@ type fun_type =
 | IntT
 | BoolT
 | ArrowT of fun_type * fun_type
-| UnknownT of fun_type option ref
+| UnknownT
 
 type context = fun_type SMap.t
 type env = context
 
-type op = 
+type op =
   | Plus
   | Minus
   | Times
@@ -54,45 +54,6 @@ let if_ c t e = If (c, t, e)
 let let_ x v b = Let (x, v, b)
 let letfun f x funtype body b = LetFun (f, x, funtype, body, b)
 
-let fresh_unknown () = UnknownT (ref None)
-
-let rec resolve_type t =
-  match t with
-  | IntT -> IntT
-  | BoolT -> BoolT
-  | ArrowT (param, ret) -> ArrowT (resolve_type param, resolve_type ret)
-  | UnknownT r ->
-      (match !r with
-       | Some t' ->
-           let resolved = resolve_type t' in
-           r := Some resolved;
-           resolved
-       | None -> t)
-
-let rec unify_types t1 t2 =
-  let t1 = resolve_type t1 in
-  let t2 = resolve_type t2 in
-  match t1, t2 with
-  | IntT, IntT -> Some IntT
-  | BoolT, BoolT -> Some BoolT
-  | ArrowT (p1, r1), ArrowT (p2, r2) ->
-      (match unify_types p1 p2, unify_types r1 r2 with
-       | Some _, Some _ -> Some (ArrowT (resolve_type p1, resolve_type r1))
-       | _ -> None)
-  | UnknownT r, t
-  | t, UnknownT r ->
-      (match !r with
-       | Some existing -> unify_types existing t
-       | None ->
-           r := Some t;
-           Some t)
-  | _ -> None
-
-let types_compatible t1 t2 =
-  match unify_types t1 t2 with
-  | Some _ -> true
-  | None -> false
-
 let rec typecheck (t: term) (g: context): fun_type option =
   match t with
   | Int n -> Some (IntT)
@@ -104,50 +65,32 @@ let rec typecheck (t: term) (g: context): fun_type option =
      | None -> None)
   | App (f, arg) ->
     (match typecheck f g, typecheck arg g with
-     | Some (ArrowT (param_type, return_type)), Some arg_type when types_compatible param_type arg_type ->
+     | Some (ArrowT (param_type, return_type)), Some arg_type when param_type = arg_type ->
         Some return_type
      | _ -> None)
   | Op (o, t1, t2) ->
     (match typecheck t1 g, typecheck t2 g with
-     | Some ty1, Some ty2 when (o = Plus || o = Minus || o = Times) && types_compatible ty1 IntT && types_compatible ty2 IntT ->
-        Some IntT
-     | Some ty1, Some ty2 when (o = And || o = Or) && types_compatible ty1 BoolT && types_compatible ty2 BoolT ->
-        Some BoolT
-     | Some ty1, Some ty2 when o = Minor && types_compatible ty1 IntT && types_compatible ty2 IntT ->
-        Some BoolT
-     | _ -> None)
+        | Some IntT, Some IntT when o = Plus || o = Minus || o = Times -> Some IntT
+        | Some BoolT, Some BoolT when o = And || o = Or -> Some BoolT
+        | Some IntT, Some IntT when o = Minor -> Some BoolT
+        | _ -> None)        
   | Not t ->
     (match typecheck t g with
-     | Some ty when types_compatible ty BoolT -> Some BoolT
+     | Some BoolT -> Some BoolT
      | _ -> None)
   | If (c, t, e) ->
     (match typecheck c g, typecheck t g, typecheck e g with
-     | Some cond_type, Some t_type, Some e_type
-       when types_compatible cond_type BoolT && types_compatible t_type e_type ->
-         Some (resolve_type t_type)
+     | Some cond_type, Some t_type, Some e_type when cond_type = BoolT && t_type = e_type -> Some t_type
      | _ -> None)
   | Let (x, v, b) ->
     (match typecheck v g with
      | Some v_type -> typecheck b (SMap.add x v_type g)
      | None -> None)
   | LetFun (f, x, param_type, body, b) ->
-    let return_placeholder = fresh_unknown () in
-    let fun_type = ArrowT (param_type, return_placeholder) in
-    let env_with_fun = SMap.add f fun_type g in
-    (match typecheck body (SMap.add x param_type env_with_fun) with
-     | Some body_type when types_compatible return_placeholder body_type ->
-         let resolved_return = resolve_type return_placeholder in
-         (match resolved_return with
-          | UnknownT _ -> None
-          | _ ->
-              let resolved_fun = ArrowT (resolve_type param_type, resolved_return) in
-              typecheck b (SMap.add f resolved_fun g))
-     | _ -> None)
-let typecheck_term (t: term): fun_type option =
-  match typecheck t SMap.empty with
-  | Some ty ->
-      let resolved = resolve_type ty in
-      (match resolved with
-       | UnknownT _ -> None
-       | _ -> Some resolved)
-  | None -> None
+    let fun_type = ArrowT (param_type, UnknownT) in
+    let extended_g = SMap.add f fun_type (SMap.add x param_type g) in
+    (match typecheck body extended_g with
+     | Some body_type ->
+        let resolved_fun_type = ArrowT (param_type, body_type) in
+        typecheck b (SMap.add f resolved_fun_type g)
+     | None -> None)
